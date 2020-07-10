94a27015ca7d141fbbfc25a31f9b57fcd20e1729
Matthew Du Pont <github@matthewfdupont.com>
2020-07-10 06:31:13 -0700

    cli parsing

--- composer.py
+++ composer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import csv
+import sys, getopt
 
 def emacsify(rows):
 	output_text = ''
@@ -12,7 +13,31 @@ def emacsify(rows):
 
 	return output_text
 
-def main():
+def parse_cli(argv):
+
+	help_string = './composer.py\n-p <priority: ["red"|"yellow"|"green"]>\n-d <daytype: ["workday"|"weekend"]>'
+	arguments = {}
+
+	try:
+		opts, args = getopt.getopt(argv,'hp:d:',['priority=','day_type='])
+	except getopt.GetoptError:
+		print(help_string)
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print(help_string)
+			sys.exit()
+		elif opt in ("-p", "--priority"):
+			arguments["priority"] = arg
+		elif opt in ("-d", "--daytype"):
+			arguments["day_type"] = arg
+	
+	return arguments
+
+def main(argv):
+
+	arguments = parse_cli(argv)
+
 	csv_reader = ''
 	selected_rows = []
 
@@ -27,4 +52,5 @@ def main():
 	with open('./todays_routine.org', 'w') as file:
 		file.write(formatted_output)
 
-main()
+# needed to actually run the program
+main(sys.argv[1:])
