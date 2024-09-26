1fa2ee6c4572fed124ef03486fa63d845e5f089a
Matt Du Pont <mattdp@users.noreply.github.com>
2024-09-26 10:19:22 -0400

    refactor so easier exhortation editing

--- sample_inputs/instructions.py
+++ sample_inputs/instructions.py
@@ -7,7 +7,7 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 from task import Task
 
-MORNING_WORK_RITUAL = [
+morning_start = [
     "DEPTH_DEFAULT",
     Task(title="Morning work routine",tags=["work"]),
     "DEPTH_DOWN",
@@ -19,6 +19,9 @@ MORNING_WORK_RITUAL = [
     "DEPTH_UP",
     Task(title="Exhortations"),
     "DEPTH_DOWN",
+]
+
+morning_exhortations = [
     Task(title="Focus"),
     "DEPTH_DOWN",
     Task(title="To achieve broader goals around being curious and living life well, you need to efficiently trade a limited amount of your time for a high amount of money."),
@@ -62,7 +65,10 @@ MORNING_WORK_RITUAL = [
     "DEPTH_DOWN",
     Task(title="Most of sales isn't hard. As AI improves, more of your value will be in who you know."),            
     Task(title="Spending time talking to people you genuninely like is a lot more fun than cold calling."),
-    "DEPTH_UP", 
+    "DEPTH_UP"
+]
+
+morning_close = [
     "DEPTH_UP", 
     Task(title="Triage"),
     "DEPTH_DOWN",
@@ -74,38 +80,7 @@ MORNING_WORK_RITUAL = [
     Task(title="Improvement: select what aiming to improve today with [#A] tag")
 ]
 
-SALES_ROUTINE_TYPICAL = [
-    "DEPTH_DEFAULT",
-    Task(title="Daily workflow", tags=["work"]),
-    "DEPTH_DOWN",        
-    Task(title="Prep meetings that'll happen before deep work done"),
-    Task(title="On call?"),
-    "DEPTH_DOWN",        
-    Task(title="If on call, turn on #livechat and respond to today's leads"),        
-    Task(title="Hubspot Live Chats"),
-    Task(title="Hubspot contact forms", note="https://app.hubspot.com/submissions/8863617/form/e194656e-6583-4896-a134-37e608428d0e/submissions/?redirectUrl=https%3A%2F%2Fapp.hubspot.com%2Fforms%2F8863617"),
-    Task(title="sales@ emails"),
-    "DEPTH_UP",           
-    Task(title="Deep work on main goals for day"),
-    Task(title="'1 few hours' email"),
-    Task(title="Prep later meetings for today"),
-    Task(title="Tier 0 GTD"),
-    Task(title="SSS give 5 tacos and post my progress"),
-    Task(title="15 minutes impactful prospecting"),
-    Task(title="'2 within BD' email"),
-    Task(title="Tier 1 GTD"),
-    Task(title="Full pomodoro of personal or sales learning", note="https://keep.google.com/"),
-    Task(title="Email remind people of convos in 2 biz days, if not a recent booking"),
-    Task(title="Prep tomorrow's meetings"),
-    Task(title="Full pomodoro of personal admin/todos", note="https://keep.google.com/"),
-    Task(title="15 more minutes prospecting"),
-    Task(title="Tier 2 GTD"),
-    Task(title="Prep tomorrow's meetings"),
-    Task(title="Prospect at least 1 hour"),
-    Task(title="Tier 3 + 4 GTD")
-]
-
-always_start = [
+sales_start = [
     "DEPTH_DEFAULT",
     Task(title="Daily workflow", tags=["work"]),
     "DEPTH_DOWN",        
@@ -119,7 +94,7 @@ always_start = [
     "DEPTH_UP"
 ]    
 
-arbitrary_order = [
+sales_arbitrary_order = [
     Task(title="Deep work on main goals for day"),
     Task(title="'1 few hours' email"),
     Task(title="Prep later meetings for today"),
@@ -132,11 +107,11 @@ arbitrary_order = [
     Task(title="Email remind people of convos in 2 biz days, if not a recent booking"),
 ]
 if random.randint(1,10) >= 6:
-    arbitrary_order += [Task(title="Go do something fun for 15 minutes!")]
+    sales_arbitrary_order += [Task(title="Go do something fun for 15 minutes!")]
 
-random.shuffle(arbitrary_order)
+random.shuffle(sales_arbitrary_order)
 
-closing = [
+sales_closing = [
     Task(title="Prep tomorrow's meetings"),
     Task(title="Full pomodoro of personal admin/todos", note="https://keep.google.com/"),
     Task(title="15 more minutes prospecting"),
@@ -149,5 +124,6 @@ closing = [
 class Instructions:
 
     def instructions_list():
-        instructions = MORNING_WORK_RITUAL + always_start + arbitrary_order + closing
-        return instructions
+        morning_routine = morning_start + morning_exhortations + morning_close
+        sales_routine = sales_start + sales_arbitrary_order + sales_closing
+        return morning_routine + sales_routine
